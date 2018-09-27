e3d35a2fa95fedcb23ad615e5140f5c006d62bd7
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-09-27 21:05:32 +0200

    added CalculatorController

---
+++ RestCalculatorService/Controllers/CalculatorController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCalculatorService.Model;
+
+namespace RestCalculatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET: api/Calculator
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Calculator/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Calculator
+        [HttpPost("{Add}", Name = "Add" ) ]
+        public int Post([FromBody] Data data)
+        {
+            return data.A + data.B;
+        }
+
+        // PUT: api/Calculator/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
